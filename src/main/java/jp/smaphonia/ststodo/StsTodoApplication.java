35f6b9f73bfb49a00ccf2e355da3cc822e160d02
Masaaki Uno <unokun@gmail.com>
2019-04-28 15:16:12 +0900

    create project

---
+++ src/main/java/jp/smaphonia/ststodo/StsTodoApplication.java
@@ -0,0 +1,13 @@
+package jp.smaphonia.ststodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsTodoApplication.class, args);
+	}
+
+}
